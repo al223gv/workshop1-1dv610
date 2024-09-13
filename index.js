0c464b487d2bf6466cfef126ee232a3ccd0c0dc9
Robin Pettersson <rp222nc@student.lnu.se>
2024-09-13 14:09:40 +0200

    Check for coming leap year, add comments

--- index.js
+++ index.js
@@ -11,20 +11,22 @@ let isValidYear = false
 do {
   try {
     const year = prompt('Please enter a year: ')
+    
+    if (year === '') {
+      throw Error('Enter a valid year')
+    }
     console.log('The year you entered is: ' + year)
     const isLeapYear = leapYear.determineLeapYear(year)
     console.log(`Is year a leapyear: ${isLeapYear}`)
+
+    // Log next leap year
+    if (isLeapYear) {
+      const intYear = parseInt(year)
+      console.log(`Next leap year is in: ${intYear + 4}`)
+    }
   } catch (error) {
     console.log(error)
   }
 
 } while (isValidYear)
-
-/* const year = prompt('Please enter a year: ') */
-
-/* console.log('The year you entered is: ' + year) */
-
-/* const isLeapYear = leapYear.determineLeapYear(year) */
-
-/* console.log(`Is year a leapyear: ${isLeapYear}`) */
 
--- leapYear.js
+++ leapYear.js
@@ -1,23 +1,35 @@
 export class LeapYear {
 
-    determineLeapYear (year) {
-        let leapYear = false
+  /**
+   * Checks if year is a leap year.
+   *
+   * @param {*} year
+   * @return {boolean} 
+   * @memberof LeapYear
+   */
+  determineLeapYear(year) {
+    let leapYear = false
 
-        if (this.isYear(year)) {
-          throw new Error('Your input is not a valid year.')
-        }
-      
-        if (year % 4 === 0) {
-          leapYear = true
-        }
-        return leapYear
-      }
+    if (this.isYear(year)) {
+      throw new Error('Your input is not a valid year.')
+    }
 
-    isYear (year) {
-      if (typeof year === 'number' && year >= 0) {
+    if (year % 4 === 0) {
+      leapYear = true
+    }
+    return leapYear
+  }
 
-        return true
-      }
-      return false
+  /**
+   *  Checks if eyar is a positive number 
+   * @param {*} year 
+   * @returns 
+   */
+  isYear(year) {
+    if (typeof year === 'number' && year >= 0) {
+
+      return true
+    }
+    return false
   }
 }
